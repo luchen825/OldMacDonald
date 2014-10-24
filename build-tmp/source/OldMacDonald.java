396df9585566f584ab6dc1b675017652f92f325b
luchen825 <lucychen28@gmail.com>
2014-10-24 10:21:08 -0700

    Added OldMacDonald

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-void setup()
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
+
+public void setup()
 {
 	Farm oldMac = new Farm();
 	oldMac.showAnimals();
@@ -52,7 +68,7 @@ class Chicken implements Animal
 	private String myType;
 	public Chicken(String sound1, String sound2, String type1)
 	{
-		if(Math.random() > 0.5)
+		if(Math.random() > 0.5f)
 		{
 			mySound = sound1;
 		}
@@ -106,4 +122,13 @@ class Farm
 		}
 		System.out.println("The cow is named " + ((NamedCow)someAnimals[0]).getName() + ".");
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
